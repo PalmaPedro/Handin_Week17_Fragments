0cd04582c6a00a4c568650102635dadb015e3566
Pedro Palma <pmcpalma@gmail.com>
2020-05-10 15:26:20 +0200

    Add files via upload

--- src/main/java/com/pedropalma/handin_week17_fragments/MainActivity.java
+++ src/main/java/com/pedropalma/handin_week17_fragments/MainActivity.java
@@ -1,14 +1,24 @@
 package com.pedropalma.handin_week17_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentDetails fd = new FragmentDetails();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment2, fd);
+        ft.commit();
     }
+
+
 }
